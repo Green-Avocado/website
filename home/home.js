9823243f9d56954dc0d28b761e8113fd0285cb71
Green-Avocado <greenavocado@protonmail.com>
2020-12-15 01:08:38 -0800

    feat: drop bad hosts, added localhost mode

--- home/home.js
+++ home/home.js
@@ -2,6 +2,36 @@
 
 const ejs = require('ejs');
 const express = require('express');
+const yargs = require('yargs');
+
+
+
+const PORT = 5000;
+
+var allowedHosts = [
+    'jasonn.dev',
+    'www.jasonn.dev'
+]
+
+
+
+const argv = yargs
+    .option('local', {
+        alias: 'l',
+        description: 'Allow requests to localhost',
+        type: 'boolean',
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+if (argv.local) {
+    console.log(`Running on localhost:${PORT}`);
+
+    allowedHosts = [`localhost:${PORT}`];
+}
+
+
 
 const app = express();
 
@@ -17,9 +47,31 @@ function serverlog(req, code) {
     );
 }
 
+
+
+app.get('*', function(req, res, next) {
+    if(allowedHosts.includes(req.get('host')))
+    {
+        next();
+    }
+    else
+    {
+        const res_code = 400;
+        serverlog(req, res_code);
+
+        const res_msg = 'Bad Request';
+        res.status(res_code);
+
+        res.type('txt').send(res_msg);
+        return;
+    }
+});
+
+
+
 app.get('/', function(req, res) {
     const res_code = 200;
-    serverlog(req, res_code)
+    serverlog(req, res_code);
 
     res.render('pages/index');
 });
@@ -28,21 +80,21 @@ app.get('/', function(req, res) {
 
 app.get('/about', function(req, res) {
     const res_code = 200;
-    serverlog(req, res_code)
+    serverlog(req, res_code);
 
     res.render('pages/about');
 });
 
 app.get('/services', function(req, res) {
     const res_code = 200;
-    serverlog(req, res_code)
+    serverlog(req, res_code);
 
     res.render('pages/services');
 });
 
 app.get('/contact', function(req, res) {
     const res_code = 200;
-    serverlog(req, res_code)
+    serverlog(req, res_code);
 
     res.render('pages/contact');
 });
@@ -55,7 +107,7 @@ app.use(express.static('./resources'));
 
 app.use('*', function(req, res) {
     const res_code = 404;
-    serverlog(req, res_code)
+    serverlog(req, res_code);
 
     const res_msg = 'Not found';
     res.status(res_code);
@@ -75,7 +127,7 @@ app.use('*', function(req, res) {
 
 
 
-app.listen(5000);
+app.listen(PORT);
 
 process.on('SIGINT', function() {
     console.log('\nGracefully shutting down from SIGINT (Ctrl-C)\n');
