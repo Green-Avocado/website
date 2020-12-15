01e0a860395464f747bd1e79e55cd78695044389
Green-Avocado <greenavocado@protonmail.com>
2020-12-14 16:31:07 -0800

    feat: moved server files to parent directories

---
+++ home/home.js
@@ -0,0 +1,49 @@
+const ejs = require('ejs');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+function serverlog(req, code, msg) {
+    console.log(
+        new Date,
+        'Request: ' + req.protocol + '://' + req.get('host') + req.originalUrl,
+        'Response: (' + code + ') ' + msg
+    );
+}
+
+app.use("*", function(req, res, next) {
+    const res_msg = 'pending';
+    const res_code = 'pending';
+
+    serverlog(req, res_code, res_msg);
+    next();
+});
+
+app.get('*', function(req, res) {
+    const res_msg = 'Not found';
+    const res_code = 404;
+
+    serverlog(req, res_code, res_msg)
+    res.status(res_code);
+
+    if(req.accepts('html')) {
+        res.sendFile(path.join(__dirname + './resources/404.html'));
+        return ;
+    }
+
+    if(req.accepts('json')) {
+        res.json({ error: res_msg });
+        return;
+    }
+
+    res.type('txt').send(res_msg);
+});
+
+app.listen(3001);
+
+process.on('SIGINT', function() {
+    console.log('\nGracefully shutting down from SIGINT (Ctrl-C)\n');
+    process.exit();
+});
+
